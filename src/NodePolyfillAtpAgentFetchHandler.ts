c6786db7349b445fd818ac33e5f8855bd8ce306d
R.G. Wood <ric@grislyeye.com>
2023-12-27 18:34:26 +0000

    Formatting

--- src/NodePolyfillAtpAgentFetchHandler.ts
+++ src/NodePolyfillAtpAgentFetchHandler.ts
@@ -1,19 +1,30 @@
-import { AtpAgentFetchHandler, AtpAgentFetchHandlerResponse } from '@atproto/api'
-import nodeFetch from 'node-fetch'
+import {
+  AtpAgentFetchHandler,
+  AtpAgentFetchHandlerResponse,
+} from "@atproto/api";
+import nodeFetch from "node-fetch";
 
-export const NodePolyfillAtpAgentFetchHandler: AtpAgentFetchHandler =
-  async (httpUri: string, httpMethod: string, httpHeaders: Record<string, string>, httpReqBody: any) =>  {
-    const response = await nodeFetch(httpUri, {
-      method: httpMethod,
-      headers: httpHeaders,
-      body: JSON.stringify(httpReqBody)
-    })
-    if(response.status < 200 || response.status >= 300) {
-      throw new Error(`Invalid response (${response.status}) message=[${await response.text()}]`)
-    }
-    const atpResponse = {
-      status: response.status,
-      body: await response.json()
-    } as AtpAgentFetchHandlerResponse
-    return atpResponse
+export const NodePolyfillAtpAgentFetchHandler: AtpAgentFetchHandler = async (
+  httpUri: string,
+  httpMethod: string,
+  httpHeaders: Record<string, string>,
+  httpReqBody: any,
+) => {
+  const response = await nodeFetch(httpUri, {
+    method: httpMethod,
+    headers: httpHeaders,
+    body: JSON.stringify(httpReqBody),
+  });
+  if (response.status < 200 || response.status >= 300) {
+    throw new Error(
+      `Invalid response (${
+        response.status
+      }) message=[${await response.text()}]`,
+    );
   }
+  const atpResponse = {
+    status: response.status,
+    body: await response.json(),
+  } as AtpAgentFetchHandlerResponse;
+  return atpResponse;
+};
--- src/Valid.ts
+++ src/Valid.ts
@@ -1,25 +1,24 @@
-type Store = (key: string) => string | undefined
+type Store = (key: string) => string | undefined;
 
 class ValidT {
-  private readonly key: string
+  private readonly key: string;
 
   constructor(key: string) {
-    this.key = key
+    this.key = key;
   }
 
   required(store: Store): string {
-    const value = store(this.key)
-    if (value) return value
-    else throw new Error(`${this.key} is required`)
+    const value = store(this.key);
+    if (value) return value;
+    else throw new Error(`${this.key} is required`);
   }
 
   as<T>(store: Store, parser: (s: string) => T | undefined): T | undefined {
-    const value = store(this.key)
-    if (value) return parser(value)
+    const value = store(this.key);
+    if (value) return parser(value);
   }
-
 }
 
 export function Valid(key: string): ValidT {
-  return new ValidT(key)
+  return new ValidT(key);
 }
